97fa0e2c386c03b8619fdcc38f8e9391a3c9890a
Zeldina Chalco <dazelcolab2016@gmail.com>
2016-12-20 10:22:47 -0500

    run ejercicio-2

---
+++ main.js
@@ -0,0 +1,11 @@
+var numero1, numero2, nuemero3,promedio, suma;
+numero1 = prompt("Por favor ingresa un numero");
+numero2 = prompt("Por favor ingresa otro numero");
+numero3 = prompt("Por favor ingresa otro numero");
+
+suma = parseInt(numero1) + parseInt(numero2) + parseInt(numero3);
+promedio = suma/3;
+alert (promedio);
+
+
+
